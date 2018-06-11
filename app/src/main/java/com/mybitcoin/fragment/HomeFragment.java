909af513841e5d93ad0a744d74a7819fc7a2d620
VinitYadav <vinityadav1991@gmail.com>
2018-06-11 14:00:23 +0530

    Add graph

--- app/src/main/java/com/mybitcoin/fragment/HomeFragment.java
+++ app/src/main/java/com/mybitcoin/fragment/HomeFragment.java
@@ -3,13 +3,35 @@ package com.mybitcoin.fragment;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.anychart.anychart.AnyChart;
+import com.anychart.anychart.Cartesian;
+import com.anychart.anychart.CartesianSeriesLine;
+import com.anychart.anychart.DataEntry;
+import com.anychart.anychart.EnumsAnchor;
+import com.anychart.anychart.Mapping;
+import com.anychart.anychart.MarkerType;
+import com.anychart.anychart.Set;
+import com.anychart.anychart.Stroke;
+import com.anychart.anychart.TooltipPositionMode;
+import com.anychart.anychart.ValueDataEntry;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.mybitcoin.R;
 import com.mybitcoin.databinding.FragmentHomeBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
 
 
 public class HomeFragment extends Fragment {
@@ -31,27 +53,126 @@ public class HomeFragment extends Fragment {
         return mBinding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //initGraph();
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 7000),
+                new DataPoint(1, 7003),
+                new DataPoint(2, 7002),
+                new DataPoint(3, 7001),
+                new DataPoint(4, 7004)
+        });
+        mBinding.graph.addSeries(series);
+    }
+
     /**
      * Initialize graph
      */
     private void initGraph() {
-        /*ValueLineSeries series = new ValueLineSeries();
-        series.setColor(0xFF56B7F1);
+        Cartesian cartesian = AnyChart.line();
 
-        series.addPoint(new ValueLinePoint("Jan", 2.4f));
-        series.addPoint(new ValueLinePoint("Feb", 3.4f));
-        series.addPoint(new ValueLinePoint("Mar", .4f));
-        series.addPoint(new ValueLinePoint("Apr", 1.2f));
-        series.addPoint(new ValueLinePoint("Mai", 2.6f));
-        series.addPoint(new ValueLinePoint("Jun", 1.0f));
-        series.addPoint(new ValueLinePoint("Jul", 3.5f));
-        series.addPoint(new ValueLinePoint("Aug", 2.4f));
-        series.addPoint(new ValueLinePoint("Sep", 2.4f));
-        series.addPoint(new ValueLinePoint("Oct", 3.4f));
-        series.addPoint(new ValueLinePoint("Nov", .4f));
-        series.addPoint(new ValueLinePoint("Dec", 1.3f));
+        cartesian.setAnimation(true);
 
-        mBinding.lineChart.addSeries(series);
-        mBinding.lineChart.startAnimation();*/
+        cartesian.setPadding(10d, 20d, 5d, 20d);
+
+        cartesian.getCrosshair().setEnabled(true);
+        cartesian.getCrosshair()
+                .setYLabel(true)
+                .setYStroke((Stroke) null, null, null, null, null);
+
+        cartesian.getTooltip().setPositionMode(TooltipPositionMode.POINT);
+
+        //cartesian.setTitle("Market Price.");
+
+        cartesian.getYAxis().setTitle("USD");
+        cartesian.getXAxis().getLabels().setPadding(5d, 5d, 5d, 5d);
+
+        List<DataEntry> seriesData = new ArrayList<>();
+        seriesData.add(new CustomDataEntry(getDateCurrentTimeZone(1527811200),
+                3.6, 2.3, 2.8));
+        seriesData.add(new CustomDataEntry(getDateCurrentTimeZone(1528329600),
+                7.1, 4.0, 4.1));
+        seriesData.add(new CustomDataEntry(getDateCurrentTimeZone(1528416000),
+                8.5, 6.2, 5.1));
+        seriesData.add(new CustomDataEntry(getDateCurrentTimeZone(1528502400),
+                9.2, 11.8, 6.5));
+        seriesData.add(new CustomDataEntry(getDateCurrentTimeZone(1528588800),
+                10.1, 13.0, 12.5));
+
+        Set set = new Set(seriesData);
+        Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+        //Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+        //Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
+
+        CartesianSeriesLine series1 = cartesian.line(series1Mapping);
+        series1.setName("Market Price");
+        series1.getHovered().getMarkers().setEnabled(true);
+        series1.getHovered().getMarkers()
+                .setType(MarkerType.CIRCLE)
+                .setSize(4d);
+        series1.getTooltip()
+                .setPosition("right")
+                .setAnchor(EnumsAnchor.LEFT_CENTER)
+                .setOffsetX(5d)
+                .setOffsetY(5d);
+
+        /*CartesianSeriesLine series2 = cartesian.line(series2Mapping);
+        series2.setName("Whiskey");
+        series2.getHovered().getMarkers().setEnabled(true);
+        series2.getHovered().getMarkers()
+                .setType(MarkerType.CIRCLE)
+                .setSize(4d);
+        series2.getTooltip()
+                .setPosition("right")
+                .setAnchor(EnumsAnchor.LEFT_CENTER)
+                .setOffsetX(5d)
+                .setOffsetY(5d);*/
+
+        /*CartesianSeriesLine series3 = cartesian.line(series3Mapping);
+        series3.setName("Tequila");
+        series3.getHovered().getMarkers().setEnabled(true);
+        series3.getHovered().getMarkers()
+                .setType(MarkerType.CIRCLE)
+                .setSize(4d);
+        series3.getTooltip()
+                .setPosition("right")
+                .setAnchor(EnumsAnchor.LEFT_CENTER)
+                .setOffsetX(5d)
+                .setOffsetY(5d);*/
+
+        cartesian.getLegend().setEnabled(true);
+        cartesian.getLegend().setFontSize(13d);
+        cartesian.getLegend().setPadding(0d, 0d, 10d, 0d);
+
+        mBinding.chartView.setChart(cartesian);
+    }
+
+    private class CustomDataEntry extends ValueDataEntry {
+
+        CustomDataEntry(String x, Number value, Number value2, Number value3) {
+            super(x, value);
+            //setValue("value2", value2);
+            //setValue("value3", value3);
+        }
+    }
+
+    /**
+     * Get date time
+     */
+    private String getDateCurrentTimeZone(long timestamp) {
+        try {
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.setTimeInMillis(timestamp * 1000);
+            calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date currenTimeZone = (Date) calendar.getTime();
+            return sdf.format(currenTimeZone);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
